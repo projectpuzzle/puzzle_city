cc5de29fc1d3e0666d6e9707193dced9f046e777
dassyl <dassyl.bellagh@etu.u-pec>
2020-03-13 10:31:41 +0100

    commit connector

--- src/puzzle_city_connectionPool/TestFile.java
+++ src/puzzle_city_connectionPool/TestFile.java
@@ -4,10 +4,10 @@ public class TestFile {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		DataSource d = new DataSource();
-		 d.showTest();
-		// d.addTest("anass", "lka" ,200);
+		 //d.showTest();
+		 d.addTest("azouz", "sowa" ,201);
 		//d.deleteTestByDB(200);
 		// d.updateTest("test", "test", 10);
-
+		 
 	}
 }
