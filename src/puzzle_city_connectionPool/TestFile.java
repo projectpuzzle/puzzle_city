0cf0a1b27d2f72e3e541c2eea35f2aed6ee3d92b
Hamza-1998 <talssi.hamza98@gmail.com>
2020-03-27 16:55:07 +0100

    socket

--- src/puzzle_city_connectionPool/TestFile.java
+++ src/puzzle_city_connectionPool/TestFile.java
@@ -1,13 +1,26 @@
 package puzzle_city_connectionPool;
 
+import java.io.IOException;
+
+import puzzle_city_client.Client;
+import puzzle_city_server.Server;
+
+
 public class TestFile {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		DataSource d = new DataSource();
-		//d.showTest();
-		//d.addTest( "shaun", "shaun1", 5);
-		//d.deleteTestByDB(201);
-		//d.updateTest("testupdate", "testupdate1",2);
-		 
+	//DataSource d = new DataSource();
+//		d.showTest();
+		// d.addTest( "ha", "ha1", 4);
+		// d.deleteTestByDB(201);
+		// d.updateTest("testupdate", "testupdate1",2);
+		
+		
+		
+		Server server = new Server(4000);
+	
+	
+	
+
 	}
 }
