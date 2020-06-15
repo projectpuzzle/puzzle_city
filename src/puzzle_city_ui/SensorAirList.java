2d7ee8603cea4accbac79a7bdc57f665f196826b
Hamza-1998 <talssi.hamza98@gmail.com>
2020-06-16 00:15:04 +0100

    "commit"

--- src/puzzle_city_ui/SensorAirList.java
+++ src/puzzle_city_ui/SensorAirList.java
@@ -20,7 +20,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
